1d163e1a1ecf0e2555bc9099b2ecde22be9aef38
Jesus Millan <jgmc3012@gmail.com>
2020-09-17 19:12:32 -0400

    :beetle:[FIX] test urls

--- config/api_router.py
+++ config/api_router.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 app_name = "api"
 urlpatterns = [
-    path('users', include('sentinel_users.users.api.urls')),
+    path('users/', include('sentinel_users.users.api.urls')),
 ]
